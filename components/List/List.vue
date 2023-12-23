e99dbe16cbd1195beebf457cde0d67508d35b42e
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2023-12-23 07:37:58 +0100

    chore: commit stash

--- components/List/List.vue
+++ components/List/List.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
-import type { ListItem } from '~/types'
+import type { ListItem, ListItemLink } from '~/types'
 
 const { list } = defineProps<{
   list: ListItem[]
 }>()
+
+function isLink(item: ListItem): item is ListItemLink {
+  return (<ListItemLink>item).href !== undefined
+}
 </script>
 
 <template>
@@ -11,18 +15,21 @@ const { list } = defineProps<{
     <template #item.name="{ item, level }">
       <span
         v-if="level > 0"
-        class="mx-2 inline-flex h-1 w-4 self-center rounded bg-primary-container"
+        class="mx-2 inline-flex h-1 self-center rounded bg-primary-container"
       />
       {{ item.name }}
     </template>
     <template #children="{ item, level }">
-      <div v-for="child in item.children" :key="child.name">
-        <div v-if="level > 0" class="inline-flex">
-          <Icon
-            name="ic:baseline-arrow-right"
-            class="mx-2 inline-flex h-1 w-4 self-center text-on-surface"
-          />
-        </div>
+      <div class="flex flex-col">
+        <NuxtLink v-for="child in item.children" :to="(<ListItemLink>child).href" :key="child.name">
+          <div v-if="level > 0" class="inline-flex">
+            <Icon
+              name="ic:baseline-arrow-right"
+              class="mx-2 inline-flex h-1 w-4 self-center text-on-surface"
+            />
+            {{ child.name }}
+          </div>
+        </NuxtLink>
       </div>
     </template>
   </AbstractList>
